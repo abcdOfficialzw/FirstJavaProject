7f5a92bacd205ef93d717c62c016a5150155a9c7
abcdOfficial <takutitus@icloud.com>
2023-01-11 11:41:45 +0200

    Initial commit

---
+++ Car.java
@@ -0,0 +1,10 @@
+public class Car {
+    private  long mileage;
+    public long getMileage(){
+        return mileage;
+    }
+
+    public void setMileage(long mileage){
+        this.mileage = mileage;
+    }
+}
---
+++ CarTest.java
@@ -0,0 +1,7 @@
+public class CarTest {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.setMileage(200);
+        System.out.println(car.getMileage());
+    }
+}
